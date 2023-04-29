1c2e4f9c2075912b5ff714ce40a16b783bf6ceed
Muhammed Noufal N <muhammednoufaln369@gmail.com>
2023-04-29 23:37:11 +0530

    orre todo react app ondakki vachitte onde eni athe manasilakanam eggana indakiyathnne

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
+import { useState } from 'react';
 function App() {
+
+  const [toDos, setTodos] = useState([])
+  const [toDo, setTodo] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setTodos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {toDos.map((value) => {
+          return (
+            <div className="todo">
+              <div className="left">
+                <input onChange={(e) => {
+                  console.log(e.target.checked)
+                  console.log(value)
+                  setTodos(toDos.filter(value2 => {
+                    if (value2.id === value.id) {
+                      value2.status = e.target.checked
+                    }
+                    return value2
+                  }))
+
+                }} value={value.status} type="checkbox" name="" id="" />
+                <p>{value.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times"></i>
+              </div>
+            </div>
+          )
+        })
+        }
+        <h1 style={{color:"red"}}>job done</h1>
+        {toDos.map((value)=>{
+          if(value.status)
+          {
+            return(<h3 style={{color:'white'}}>{value.text}</h3>)
+          }
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
+
